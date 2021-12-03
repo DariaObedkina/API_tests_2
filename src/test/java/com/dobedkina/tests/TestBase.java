1f9cef72ff147bbe0a0d8c6204e55e6d65af598a
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-12-03 23:50:00 +0300

    Added capabilities for selenide

--- src/test/java/com/dobedkina/tests/TestBase.java
+++ src/test/java/com/dobedkina/tests/TestBase.java
@@ -2,12 +2,18 @@ package com.dobedkina.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class TestBase {
     @BeforeAll
     public static void setUp() {
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 }
